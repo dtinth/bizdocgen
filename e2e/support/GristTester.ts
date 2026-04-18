c3714032d3dd7dbbef605d8681c610a306da71fe
dtinth on MBP M1 <dtinth@spacet.me>
2026-04-18 22:30:33 +0700

    fix formatting

--- e2e/support/GristTester.ts
+++ e2e/support/GristTester.ts
@@ -1,33 +1,33 @@
-import { Locator, Page } from "@playwright/test";
-import { PageObject } from "./PageObject";
+import { Locator, Page } from '@playwright/test'
+import { PageObject } from './PageObject'
 
 export class GristTester extends PageObject {
   constructor(page: Page) {
-    super({ page });
+    super({ page })
   }
 
   widget(title: string) {
-    const { page } = this;
-    const widgetLocator = this.page.locator(".view_leaf").filter({
-      has: this.page.locator(".viewsection_title").filter({ hasText: title }),
-    });
+    const { page } = this
+    const widgetLocator = this.page.locator('.view_leaf').filter({
+      has: this.page.locator('.viewsection_title').filter({ hasText: title }),
+    })
     const record = (recordLocator: Locator) => {
-      const fieldsLocator = recordLocator.locator(".field");
-      const selectedFieldLocator = fieldsLocator.filter({ has: page.locator(".active_cursor") });
+      const fieldsLocator = recordLocator.locator('.field')
+      const selectedFieldLocator = fieldsLocator.filter({ has: page.locator('.active_cursor') })
       return {
         locator: recordLocator,
         fieldsLocator: fieldsLocator,
         selectedFieldLocator: selectedFieldLocator,
-      };
-    };
+      }
+    }
     return {
       locator: widgetLocator,
       get newRecord() {
-        return record(widgetLocator.locator(".record-add"));
+        return record(widgetLocator.locator('.record-add'))
       },
       get selectedRecord() {
-        return record(page.locator(".record").filter({ has: page.locator(".active_cursor") }));
+        return record(page.locator('.record').filter({ has: page.locator('.active_cursor') }))
       },
-    };
+    }
   }
 }
